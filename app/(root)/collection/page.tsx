5fb4962b3bb6e751c0e335d35f74e590a07c565b
vedantDube <vedant3205@outlook.com>
2024-08-21 14:23:19 +0530

    fix

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -2,7 +2,7 @@ import NoResult from "@/components/shared/NoResult";
 import React from "react";
 
 const page = () => {
-  return <div>No result</div>;
+  return <div>No result hello</div>;
 };
 
 export default page;
