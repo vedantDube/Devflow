8673a96d1c41108ca0bcd493de310f8192ee40c7
vedhem999 <161755713+vedantDube@users.noreply.github.com>
2024-09-11 22:30:17 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter, Space_Grotesk } from "next/font/google";
 import type { Metadata } from "next";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import "../styles/prism.css";
 import { ThemeProvider } from "@/context/ThemeProvider";
